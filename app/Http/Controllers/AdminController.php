2d9b34131acc2d97b958b8c6a27ecd157ae5fa6b
Godishala Chandu <85674113+theuchandu@users.noreply.github.com>
2024-02-14 06:47:11 +0530

    admin controller added product functionality

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Catagory;
+use App\Models\Product;
 
 class AdminController extends Controller
 {
     
     public function view_catagory(){
 
-        return view('admin.catagory');
+        $data = Catagory::all();
+
+        return view('admin.catagory',compact('data'));
     }
 
     public function add_catagory(Request $request){
@@ -25,10 +28,50 @@ class AdminController extends Controller
     
         $data->save();    
       
-        return redirect()->back()->with('message', 'Category Added Successfully');
-    
+        return redirect()->back()->with('message', 'Category Added Successfully');    
     
     }
+
+    public function delete_catagory($id){
+        $data = Catagory::find($id);
+        $data->delete();
+        return redirect()->back()->with('message', 'Category Deleted Successfully');
+    }
+
+    public function view_product(){    
+        $catagory = Catagory::all();        
+        return view('admin.product',compact('catagory'));
+    }
+
+    public function add_product(Request $request){
+        
+        $product = new product();       
+        $product->title = $request->product_title;
+        $product->description = $request->product_description;
+        $product->price = $request->product_price;
+        $product->discount_price = $request->discount_price;
+        $product->catagory = $request->product_catagory; 
+        $product->quantity = $request->product_quantity;
+        
+        $image  = $request->image; 
+        $imagename=time().''.$image->getClientOriginalExtension();
+        $request->image->move('product', $imagename);
+        $product->image = $imagename ; 
+
+        // if ($request->hasFile('product_image')) {
+        //     $image = $request->file('product_image');
+        //     $extension = $image->getClientOriginalExtension();
+        //     $filename = time() . '.' . $extension;
+        //     $image->move(public_path('product'), $filename);
+        //     $product->product_image = $filename;
+        // }
+
+        $product->save();
+      
+        return redirect()->back()->with('message','Product Added Succesfully');
+
+    }
+
     
 
 }
